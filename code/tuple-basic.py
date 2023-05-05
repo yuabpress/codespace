b5704f017b5e5073270ae75781990fb94d5569ba
yuabpress <113014313+yuabpress@users.noreply.github.com>
2023-05-05 09:42:37 +0000

    .

---
+++ code/tuple-basic.py
@@ -0,0 +1,38 @@
+# %%
+p = 'python'
+y = 'python'
+print(p, y)
+# %%
+#  immutable: 문자열, 정수, 실수
+p[0] = 'j'
+# %%
+print(id(p), id(y))
+# %%
+t = ()
+t = tuple()
+type(t)
+# %%
+n = 1
+type(n)
+# %%
+m = (1,)
+type(m)
+# %%
+t1 = (1, 2, 3)
+t2 = (1, 2, 3)
+print(t1 == t2)
+print(id(t1), id(t2))
+print(id(t1) == id(t2))
+print(t1 is t2)
+# %%
+t1[0]
+# %%
+t1[0] = 10;
+# %%
+lst = [1, 2, 3]
+lst[1] = 20
+lst
+# %%
+
+
+
